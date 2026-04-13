5199c15e21dd0171fd79601b83c9cf69f0ed8cc0
erkanalkann-code <erkan_alkann@hotmail.com>
2026-04-13 11:16:21 +0300

    Create deals.js

---
+++ deals.js
@@ -0,0 +1,20 @@
+const deals = [
+{
+title:"AirPods Pro",
+store:"Gigantti",
+oldPrice:299,
+newPrice:199,
+discount:33,
+image:"https://images.unsplash.com/photo-1580910051074-3eb694886505",
+link:"#"
+},
+{
+title:"iPhone 15",
+store:"Verkkokauppa",
+oldPrice:999,
+newPrice:799,
+discount:20,
+image:"https://images.unsplash.com/photo-1511707171634-5f897ff02aa9",
+link:"#"
+}
+]
